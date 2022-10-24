3efec380da9c70af1219dd1793827c6f47dea7e1
Santiago Lorenzano <enderwigim@gmail.com>
2022-10-24 21:42:55 +0200

    Correct the like part and possible ads that may appear

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, \
+    ElementNotInteractableException, StaleElementReferenceException
 import os
+import random
 import time
+
+
+def try_to_click(css_selector):
+    """This function will help us to check if a button is available or if it can be click"""
+    try:
+        button = driver.find_element(By.CSS_SELECTOR, css_selector)
+    except (NoSuchElementException,
+            StaleElementReferenceException,
+            ElementNotInteractableException,
+            ElementClickInterceptedException):
+        pass
+    else:
+        button.click()
+
 
 chrome_driver_path = './ChromeDriver/chromedriver'
 service = Service(executable_path=chrome_driver_path)
@@ -47,33 +63,37 @@ driver.switch_to.window(tinder_window)
 time.sleep(10)
 # Tinder is going to ask us if we allow it to access to our location. We'll allow it
 allow = driver.find_element(By.CSS_SELECTOR, 'button[class="button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) '
-                                        'Ell Bdrs(100px) Px(24px) Px(20px)--s Py(0) Mih(40px) Pos(r) Ov(h) C(#fff) '
-                                        'Bg($c-pink):h::b Bg($c-pink):f::b Bg($c-pink):a::b Trsdu($fast) '
-                                        'Trsp($background) Bg($g-ds-background-brand-gradient) '
-                                        'button--primary-shadow StyledButton Bxsh($bxsh-btn) Fw($semibold) '
-                                        'focus-button-style W(225px) W(a) Mstart(8px)"]')
+                                             'Ell Bdrs(100px) Px(24px) Px(20px)--s Py(0) Mih(40px) Pos(r) Ov(h) '
+                                             'C(#fff) '
+                                             'Bg($c-pink):h::b Bg($c-pink):f::b Bg($c-pink):a::b Trsdu($fast) '
+                                             'Trsp($background) Bg($g-ds-background-brand-gradient) '
+                                             'button--primary-shadow StyledButton Bxsh($bxsh-btn) Fw($semibold) '
+                                             'focus-button-style W(225px) W(a) Mstart(8px)"]')
 
 allow.click()
 time.sleep(5)
 
-not_interested = driver.find_element(By.CSS_SELECTOR, 'button[aria-label="No me interesa"]')
+not_interested = driver.find_element(By.CSS_SELECTOR, "button[class='button Lts($ls-s) Z(0) CenterAlign Mx(a) "
+                                                      "Cur(p) Tt(u) Ell Bdrs(100px) Px(24px) Px(20px)--s Py(0) "
+                                                      "Mih(40px) Fw($semibold) focus-button-style W(a) "
+                                                      "C($c-ds-text-secondary) Mend(8px)']")
+# not_interested = driver.find_element(By.CSS_SELECTOR, 'button[aria-label="No me interesa"]')
 not_interested.click()
 time.sleep(5)
 
 
-
 finished = False
 while not finished:
-    try:
-        not_interested_button = driver.find_element(By.CSS_SELECTOR, "div[class='l17p5q9z']")
-    except NoSuchElementException:
-        try:
-            accept_button = driver.find_element(By.CSS_SELECTOR, "button[class='button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgi($g-ds-background-like):a'] span[class='D(b) Expand']")
-        except NoSuchElementException:
-            time.sleep(4)
-        else:
-            accept_button.click()
-            time.sleep(1)
-    else:
-        not_interested_button.click()
-
+    try_to_click("button[class='C($c-ds-icon-secondary) "
+                 "Bdc($c-ds-icon-secondary) Bdc($c-ds-icon-secondary-inverse):h "
+                 "C($c-ds-icon-secondary-inverse):h Bdrs(50%) Bds(s) Bdw(3px) Trsdu($fast) Trsp($transform) "
+                 "Rotate(-90deg):h--ml close Lh(1) Cur(p) focus-button-style']")
+    time.sleep(random.randint(1, 2))
+    try_to_click("div[class='l17p5q9z']")
+    time.sleep(random.randint(1, 2))
+    try_to_click("button[class='button Lts($ls-s) Z(0) "
+                 "CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) "
+                 "P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) "
+                 "Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a "
+                 "Bgi($g-ds-background-like):a'] span[class='D(b) Expand']")
+    time.sleep(random.randint(1, 2))
